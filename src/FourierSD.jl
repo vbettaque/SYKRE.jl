9bdf531ce193419a625de1c38694a9f924eeeacf
Valerie Bettaque <v.l.bettaque@gmail.com>
2025-04-03 18:06:47 -0400

    phase shenenigans

--- src/FourierSD.jl
+++ src/FourierSD.jl
@@ -3,16 +3,29 @@ module FourierSD
 using Plots
 using FFTW
 
-β = 1
-L = 52 - 1
+
+function G_free(β, L)
+    ωs = fftshift(fftfreq(L, 2π/Δτ))
+
+end
+
+
+
+β = 2
+L = 2^10 - 1
 Δτ = 2β / L
+
+fftfreq(L, 2π/Δτ)
+
+fftshift(fftfreq(L, 2π/Δτ))
+
 τs = collect((0:L-1) * Δτ)
 ωs = fftshift(fftfreq(L, 2π/Δτ))
 
 
 G = zeros(ComplexF64, L)
 G[1:2:L] = 2*im ./ (ωs[1:2:L])
-G_ = fft(ifftshift(G))
+G_ = fft(ifftshift(G)) / 2β
 
 plot(τs, real(G_))
 
