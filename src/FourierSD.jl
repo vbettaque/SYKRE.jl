1aa8640dd1118b85829df50f1b153e553aee75fb
Valerie Bettaque <v.l.bettaque@gmail.com>
2025-04-08 14:23:19 -0400

    more fourier stuff

--- src/FourierSD.jl
+++ src/FourierSD.jl
@@ -4,16 +4,41 @@ using Plots
 using FFTW
 
 β = 1
-L = 52 - 1
+J = 1
+q = 4
+L = 2^15
+
+
 Δτ = 2β / L
 τs = collect((0:L-1) * Δτ)
-ωs = fftshift(fftfreq(L, 2π/Δτ))
+ωs = fftfreq(L, 2π/Δτ)
 
+odd = isodd.(fftfreq(L, L))
+even = iseven.(fftfreq(L, L))
 
 G = zeros(ComplexF64, L)
-G[1:2:L] = 2*im ./ (ωs[1:2:L])
-G_ = fft(ifftshift(G))
+G[odd] = 4*im ./ ωs[odd]
 
+G_ = fft(G) ./ 2β
+Σ_ = J^2 * G_.^(q-1)
+
+Σ = 2β * ifft(Σ_)
+G[odd] = 0.5 * G[odd] + 0.5 * 4 ./ (-im * ωs[odd] - 2 * Σ[odd])
+
+G_ = fft(G) ./ 2β
+Σ_ = J^2 * G_.^(q-1)
+
+Σ = 2β * ifft(Σ_)
+G[odd] = 0.5 * G[odd] + 0.5 * 4 ./ (-im * ωs[odd] - 2 * Σ[odd])
+
+G_ = fft(G) ./ 2β
+
+plot(τs, real(G_))
+
+G = zeros(ComplexF64, L)
+G[even] = 4*im ./ ωs[even]
+G[1] = 0
+G_ = fft(G) ./ 2β
 plot(τs, real(G_))
 
 
