7181ee36011314aa845072ccd1599f3892f12106
Valerie Bettaque <v.l.bettaque@gmail.com>
2026-01-08 20:19:32 -0500

    so much plotting

--- notebooks/LDependence.jl
+++ notebooks/LDependence.jl
@@ -4,14 +4,13 @@ using SYKRE.Replicas
 using SYKRE.WeightedReplicas
 using SYKRE.TFDReplicas
 
-using Plots, DataFrames, CSV
+using CairoMakie, DataFrames, CSV, LsqFit, Printf, Latexify, LaTeXStrings
 
-
-function generate_det_plus_dependence(Ls, q, β; init_lerp = 0.5, lerp_divisor = 2, tol=1e-5, max_iters=1000)
+function generate_1R1_det_plus_dependence(Ls, q, β; init_lerp = 0.5, lerp_divisor = 2, tol=1e-5, max_iters=1000)
     syk = SYKData(1, 1, q, 1, β)
 
-    path = "data/L_dependence/"
-    filename = "dets" * "_beta" * string(β) * "_q" * string(q) * ".csv"
+    path = "data/L_dependence/1R1/"
+    filename = "dets_1R1" * "_beta" * string(β) * "_q" * string(q) * ".csv"
     file = path * filename
     !ispath(path) && mkpath(path)
     if !isfile(file)
@@ -35,45 +34,266 @@ function generate_det_plus_dependence(Ls, q, β; init_lerp = 0.5, lerp_divisor =
     end
 end
 
+function generate_1R2_det_plus_dependence(Ls, q, β; init_lerp = 0.5, lerp_divisor = 2, tol=1e-5, max_iters=1000)
+    syk = SYKData(1, 1, q, 1, β)
+
+    path = "data/L_dependence/1R2/"
+    filename = "dets_1R2" * "_beta" * string(β) * "_q" * string(q) * ".csv"
+    file = path * filename
+    !ispath(path) && mkpath(path)
+    if !isfile(file)
+        touch(file)
+        write(file, "L,det_minus,det_plus\n")
+    end
+
+    for i in eachindex(Ls)
+        L = Ls[i]
+        @info "$(i) out of $(length(Ls)): L = $(L)"
+
+        G_init = Replicas.init(2, L)
+
+        _, Σ = WeightedReplicas.schwinger_dyson(G_init, 0.5, syk; init_lerp = init_lerp, lerp_divisor = lerp_divisor, tol = tol, max_iters = max_iters)
+        pf_minus, pf_plus = WeightedReplicas.pfaffians(Σ, syk) 
+
+        df = DataFrame(L = L, det_minus = pf_minus^2, det_plus = pf_plus^2)
+
+        CSV.write(file, df, append=true)
+    end
+end
+
 Ls = 100:100:2000
-generate_det_plus_dependence(Ls, 4, 20; init_lerp = 0.01, lerp_divisor = 2, tol=1e-5, max_iters=1000)
+# generate_1R1_det_plus_dependence(Ls, 2, 1; init_lerp = 0.01, lerp_divisor = 2, tol=1e-5, max_iters=1000)
+generate_1R2_det_plus_dependence(Ls, 4, 1; init_lerp = 0.01, lerp_divisor = 2, tol=1e-5, max_iters=1000)
 
 
 
-# data_5_4 = CSV.File("data/L_dependence/pfaffians_beta5_q4.csv") |> DataFrame
-# data_10_4 = CSV.File("data/L_dependence/dets_beta10_q4.csv") |> DataFrame
-# data_20_4 = CSV.File("data/L_dependence/pfaffians_beta20_q4.csv") |> DataFrame
-
-# xs = LinRange(50, 2000, 2000)
-
-# p = plot(data_10_4[:, 1], data_10_4[:, 3], label="pf_plus")
-# plot!(data_10_4[:, 1], data_10_4[:, 4], label="pf_plus_approx")
-# plot!(xs, 55 ./ xs .+ 0.015, label="55 / L + 0.015")
-# plot!(xs, 120 ./ xs .+ 0.040, label="120 / L + 0.040")
-# ylims!(0, 1) 
-# xaxis!("1/L")
-# display(p)
 
 
+model(L, p) = p[1] ./ L.^p[2] .+ p[3]
 
-# plot(1 ./ data_20_4[:, 1], data_20_4[:, 2])
-# plot!(1 ./ data_20_4[:, 1], data_20_4[:, 3])
+data_1_4 = CSV.File("data/L_dependence/1R1/dets_1R1_beta1_q4.csv") |> DataFrame
+data_5_4 = CSV.File("data/L_dependence/1R1/dets_1R1_beta5_q4.csv") |> DataFrame
+data_10_4 = CSV.File("data/L_dependence/1R1/dets_1R1_beta10_q4.csv") |> DataFrame
+data_20_4 = CSV.File("data/L_dependence/1R1/dets_1R1_beta20_q4.csv") |> DataFrame
 
+Ls = LinRange(1, 2050, 2050)
 
+inch = 96
+pt = 4/3
+cm = inch / 2.54
 
-data_1_4 = CSV.File("data/L_dependence/dets_beta1_q4.csv") |> DataFrame
+width = 3.25inch
+height = 3 * width / 4
+font = 10pt
 
-xs = LinRange(50, 2000, 2000)
+begin
+    p0 = [1., 1., 0.]
+    fit_det = curve_fit(model, data_1_4[:, 1], data_1_4[:, 3], p0)
+    p_det = round.(fit_det.param, digits = 2)
+    fit_approx = curve_fit(model, data_1_4[:, 1], data_1_4[:, 4], p0)
+    p_approx = round.(fit_approx.param, digits = 2)
 
-p = plot(data_1_4[:, 1], data_1_4[:, 3], label="pf_plus")
-plot!(data_1_4[:, 1], data_1_4[:, 3], label="pf_plus_approx")
-plot!(xs, 0.115 ./ xs, label="0.115 / L")
-ylims!(0, 0.0012)
-xaxis!("1/L")
-title!("beta = 1, q = 4")
-display(p)
+    with_theme(theme_latexfonts()) do
+        f = Figure(size = (width, height), fontsize = font)
+        ax = Axis(f[1, 1],
+            # title = "Experimental data and exponential fit",
+            xlabel = L"$L$",
+            # ylabel = "Value",
+        )
+        scatter!(
+            ax,
+            data_1_4[:, 1],
+            data_1_4[:, 3],
+            color = Makie.wong_colors()[1],
+            label=L"$|\det(\mathcal{D}^+ \! - (\Delta \tau)^2 \, \Sigma)|$"
+        )
+        lines!(
+            ax,
+            Ls,
+            model(Ls, fit_det.param),
+            color = Makie.wong_colors()[1],
+            linestyle = :dash,
+            label= L"$\approx %$(p_det[1]) / L^{%$(p_det[2])}$"
+        )
+        scatter!(
+            ax,
+            data_1_4[:, 1],
+            data_1_4[:, 4],
+            color = Makie.wong_colors()[2],
+            label=L"$(\Delta\tau)^2 \, | \mathrm{tr}(\mathbb{1} \, \Sigma) |$"
+        )
+        lines!(
+            ax,
+            Ls,
+            model(Ls, fit_approx.param),
+            color = Makie.wong_colors()[2],
+            linestyle = :dash,
+            label= L"$\approx %$(p_approx[1]) / L^{%$(p_approx[2])}$"
+        )
+        ylims!(ax, 0, 0.0015)
+        xlims!(ax, 0, 2050)
+        axislegend(position = :rt)
+        resize_to_layout!(f)
+        save("figures/L_dependence/1R1/dets_1R1_beta1_q4.pdf", f)
+        f
+    end
+end
 
+begin
+    p0 = [1., 1., 0.]
+    fit_det = curve_fit(model, data_5_4[14:end, 1], data_5_4[14:end, 3], p0)
+    p_det = round.(fit_det.param, digits = 2)
+    fit_approx = curve_fit(model, data_5_4[5:end, 1], data_5_4[5:end, 4], p0)
+    p_approx = round.(fit_approx.param, digits = 2)
 
+    with_theme(theme_latexfonts()) do
+        f = Figure(size = (width, height), fontsize = font)
+        ax = Axis(f[1, 1],
+            # title = "Experimental data and exponential fit",
+            xlabel = L"$L$",
+            # ylabel = "Value",
+        )
+        scatter!(
+            ax,
+            data_5_4[:, 1],
+            data_5_4[:, 3],
+            color = Makie.wong_colors()[1],
+            label=L"$|\det(\mathcal{D}^+ \! - (\Delta \tau)^2 \, \Sigma)|$"
+        )
+        lines!(
+            ax,
+            Ls,
+            model(Ls, fit_det.param),
+            color = Makie.wong_colors()[1],
+            linestyle = :dash,
+            label= L"$\approx %$(p_det[1]) / L^{%$(p_det[2])}$"
+        )
+        scatter!(
+            ax,
+            data_5_4[:, 1],
+            data_5_4[:, 4],
+            color = Makie.wong_colors()[2],
+            label=L"$(\Delta\tau)^2 \, | \mathrm{tr}(\mathbb{1} \, \Sigma) |$"
+        )
+        lines!(
+            ax,
+            Ls,
+            model(Ls, fit_approx.param),
+            color = Makie.wong_colors()[2],
+            linestyle = :dash,
+            label= L"$\approx %$(p_approx[1]) / L^{%$(p_approx[2])}$"
+        )
+        ylims!(ax, 0, 0.005)
+        xlims!(ax, 0, 2050)
+        axislegend(position = :rt)
+        resize_to_layout!(f)
+        save("figures/L_dependence/1R1/dets_1R1_beta5_q4.pdf", f)
+        f
+    end
+end
 
-# plot(1 ./ data_20_4[:, 1], data_20_4[:, 2])
-# plot!(1 ./ data_20_4[:, 1], data_20_4[:, 3])
+begin
+    p0 = [1., 1., 0.]
+    fit_det = curve_fit(model, data_10_4[5:end, 1], data_10_4[5:end, 3], p0)
+    p_det = round.(fit_det.param, digits = 2)
+    fit_approx = curve_fit(model, data_10_4[5:end, 1], data_10_4[5:end, 4], p0)
+    p_approx = round.(fit_approx.param, digits = 2)
+
+    with_theme(theme_latexfonts()) do
+        f = Figure(size = (width, height), fontsize = font)
+        ax = Axis(f[1, 1],
+            # title = "Experimental data and exponential fit",
+            xlabel = L"$L$",
+            # ylabel = "Value",
+        )
+        scatter!(
+            ax,
+            data_10_4[:, 1],
+            data_10_4[:, 3],
+            color = Makie.wong_colors()[1],
+            label=L"$|\det(\mathcal{D}^+ \! - (\Delta \tau)^2 \, \Sigma)|$"
+        )
+        lines!(
+            ax,
+            Ls,
+            model(Ls, fit_det.param),
+            color = Makie.wong_colors()[1],
+            linestyle = :dash,
+            label= L"$\approx %$(p_det[1]) / L^{%$(p_det[2])}$"
+        )
+        scatter!(
+            ax,
+            data_10_4[:, 1],
+            data_10_4[:, 4],
+            color = Makie.wong_colors()[2],
+            label=L"$(\Delta\tau)^2 \, | \mathrm{tr}(\mathbb{1} \, \Sigma) |$"
+        )
+        lines!(
+            ax,
+            Ls,
+            model(Ls, fit_approx.param),
+            color = Makie.wong_colors()[2],
+            linestyle = :dash,
+            label= L"$\approx %$(p_approx[1]) / L^{%$(p_approx[2])}$"
+        )
+        ylims!(ax, 0, 1)
+        xlims!(ax, 0, 2050)
+        axislegend(position = :rt)
+        resize_to_layout!(f)
+        save("figures/L_dependence/1R1/dets_1R1_beta10_q4.pdf", f)
+        f
+    end
+end
+
+begin
+    p0 = [1., 1., 0.]
+    fit_det = curve_fit(model, data_20_4[5:end, 1], data_20_4[5:end, 3], p0)
+    p_det = round.(fit_det.param, digits = 2)
+    fit_approx = curve_fit(model, data_20_4[5:end, 1], data_20_4[5:end, 4], p0)
+    p_approx = round.(fit_approx.param, digits = 2)
+
+    with_theme(theme_latexfonts()) do
+        f = Figure(size = (width, height), fontsize = font)
+        ax = Axis(f[1, 1],
+            # title = "Experimental data and exponential fit",
+            xlabel = L"$L$",
+            # ylabel = "Value",
+        )
+        scatter!(
+            ax,
+            data_20_4[:, 1],
+            data_20_4[:, 3],
+            color = Makie.wong_colors()[1],
+            label=L"$|\det(\mathcal{D}^+ \! - (\Delta \tau)^2 \, \Sigma)|$"
+        )
+        lines!(
+            ax,
+            Ls,
+            model(Ls, fit_det.param),
+            color = Makie.wong_colors()[1],
+            linestyle = :dash,
+            label= L"$\approx %$(p_det[1]) / L^{%$(p_det[2])}$"
+        )
+        scatter!(
+            ax,
+            data_20_4[:, 1],
+            data_20_4[:, 4],
+            color = Makie.wong_colors()[2],
+            label=L"$(\Delta\tau)^2 \, | \mathrm{tr}(\mathbb{1} \, \Sigma) |$"
+        )
+        lines!(
+            ax,
+            Ls,
+            model(Ls, fit_approx.param),
+            color = Makie.wong_colors()[2],
+            linestyle = :dash,
+            label= L"$\approx %$(p_approx[1]) / L^{%$(p_approx[2])}$"
+        )
+        ylims!(ax, 0, 20)
+        xlims!(ax, 0, 2050)
+        axislegend(position = :rt)
+        resize_to_layout!(f)
+        save("figures/L_dependence/1R1/dets_1R1_beta20_q4.pdf", f)
+        f
+    end
+end
