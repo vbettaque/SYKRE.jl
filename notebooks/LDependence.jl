bb94e29e6280fea7f000ba39219eb85ca37c3469
Valerie Bettaque <v.l.bettaque@gmail.com>
2026-01-15 18:02:50 -0500

    more spikey stuff

--- notebooks/LDependence.jl
+++ notebooks/LDependence.jl
@@ -9,7 +9,7 @@ using CairoMakie, DataFrames, CSV, LsqFit, Printf, Latexify, LaTeXStrings
 function generate_1R1_det_plus_dependence(Ls, q, β; init_lerp = 0.5, lerp_divisor = 2, tol=1e-5, max_iters=1000)
     syk = SYKData(1, 1, q, 1, β)
 
-    path = "data/L_dependence/1R1/"
+    path = "data/L_dependence/det_plus/1R1/"
     filename = "dets_1R1" * "_beta" * string(β) * "_q" * string(q) * ".csv"
     file = path * filename
     !ispath(path) && mkpath(path)
@@ -37,7 +37,7 @@ end
 function generate_1R2_det_plus_dependence(Ls, q, β; init_lerp = 0.5, lerp_divisor = 2, tol=1e-5, max_iters=1000)
     syk = SYKData(1, 1, q, 1, β)
 
-    path = "data/L_dependence/1R2/"
+    path = "data/L_dependence/det_plus/1R2/"
     filename = "dets_1R2" * "_beta" * string(β) * "_q" * string(q) * ".csv"
     file = path * filename
     !ispath(path) && mkpath(path)
@@ -61,7 +61,39 @@ function generate_1R2_det_plus_dependence(Ls, q, β; init_lerp = 0.5, lerp_divis
     end
 end
 
+function generate_jump_dependence(Ls, q, β, w; init_lerp = 0.5, lerp_divisor = 2, tol=1e-5, max_iters=1000)
+    syk_1 = SYKData(1, 1, q, 1, β)
+    syk_2 = SYKData(1, 1, q, 2, β)
+
+    path = "data/L_dependence/jump/"
+    filename = "jump_beta" * string(β) * "_w" * string(w) * "_q" * string(q) * ".csv"
+    file = path * filename
+    !ispath(path) && mkpath(path)
+    if !isfile(file)
+        touch(file)
+        write(file, "L,saddle_0,saddle_w\n")
+    end
+
+    for i in eachindex(Ls)
+        L = Ls[i]
+        @info "$(i) out of $(length(Ls)): L = $(L)"
+
+        G_init_1 = Replicas.init(1, L)
+        G_init_2 = Replicas.init(2, L)
+
+        G_1, Σ_1 = WeightedReplicas.schwinger_dyson(G_init_1, 0.0, syk_1; init_lerp = init_lerp, lerp_divisor = lerp_divisor, tol = tol, max_iters = max_iters)
+        saddle_0 = 2 * WeightedReplicas.log_saddle(G_1, Σ_1, 0, syk_1)
+        G_2, Σ_2 = WeightedReplicas.schwinger_dyson(G_init_2, w, syk_2; init_lerp = init_lerp, lerp_divisor = lerp_divisor, tol = tol, max_iters = max_iters)
+        saddle_w = WeightedReplicas.log_saddle(G_2, Σ_2, w, syk_2)
+
+        df = DataFrame(L = L, saddle_0 = saddle_0, saddle_w = saddle_w)
+
+        CSV.write(file, df, append=true)
+    end
+end
+
 Ls = 100:100:2000
+generate_jump_dependence(Ls, 4, 20, 0.01; init_lerp = 0.01, lerp_divisor = 2, tol=1e-5, max_iters=1000)
 # generate_1R1_det_plus_dependence(Ls, 2, 1; init_lerp = 0.01, lerp_divisor = 2, tol=1e-5, max_iters=1000)
 # generate_1R2_det_plus_dependence(Ls, 4, 20; init_lerp = 0.01, lerp_divisor = 2, tol=1e-5, max_iters=1000)
 
@@ -71,10 +103,10 @@ Ls = 100:100:2000
 
 model(L, p) = p[1] ./ L.^p[2] .+ p[3]
 
-data_1_4 = CSV.File("data/L_dependence/1R1/dets_1R1_beta1_q4.csv") |> DataFrame
-data_5_4 = CSV.File("data/L_dependence/1R1/dets_1R1_beta5_q4.csv") |> DataFrame
-data_10_4 = CSV.File("data/L_dependence/1R1/dets_1R1_beta10_q4.csv") |> DataFrame
-data_20_4 = CSV.File("data/L_dependence/1R1/dets_1R1_beta20_q4.csv") |> DataFrame
+data_1_4 = CSV.File("data/L_dependence/det_plus/1R1/dets_1R1_beta1_q4.csv") |> DataFrame
+data_5_4 = CSV.File("data/L_dependence/det_plus/1R1/dets_1R1_beta5_q4.csv") |> DataFrame
+data_10_4 = CSV.File("data/L_dependence/det_plus/1R1/dets_1R1_beta10_q4.csv") |> DataFrame
+data_20_4 = CSV.File("data/L_dependence/det_plus/1R1/dets_1R1_beta20_q4.csv") |> DataFrame
 
 Ls = LinRange(1, 2500, 2500)
 
@@ -308,10 +340,10 @@ end
 
 model(L, p) = p[1] ./ L.^p[2] .+ p[3]
 
-data_1_4 = CSV.File("data/L_dependence/1R2/dets_1R2_beta1_q4.csv") |> DataFrame
-data_5_4 = CSV.File("data/L_dependence/1R2/dets_1R2_beta5_q4.csv") |> DataFrame
-data_10_4 = CSV.File("data/L_dependence/1R2/dets_1R2_beta10_q4.csv") |> DataFrame
-data_20_4 = CSV.File("data/L_dependence/1R2/dets_1R2_beta20_q4.csv") |> DataFrame
+data_1_4 = CSV.File("data/L_dependence/det_plus/1R2/dets_1R2_beta1_q4.csv") |> DataFrame
+data_5_4 = CSV.File("data/L_dependence/det_plus/1R2/dets_1R2_beta5_q4.csv") |> DataFrame
+data_10_4 = CSV.File("data/L_dependence/det_plus/1R2/dets_1R2_beta10_q4.csv") |> DataFrame
+data_20_4 = CSV.File("data/L_dependence/det_plus/1R2/dets_1R2_beta20_q4.csv") |> DataFrame
 
 Ls = LinRange(1, 2500, 2500)
 
